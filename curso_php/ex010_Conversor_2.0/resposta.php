e0ff05b00eeb939d6ef845654595f5abc3e9883f
Gabriel Vinícius <97963837+gabrielgvss@users.noreply.github.com>
2023-07-04 12:17:33 -0300

    Add files via upload

---
+++ curso_php/ex010_Conversor_2.0/resposta.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> CONVERSOR 1.0 </title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <header>
+        <h1> CONVERSOR DE REAL PARA DÓLAR </h1>
+
+    </header>
+    <main>
+        <?php 
+            $inicio = date("m-d-Y", strtotime("-7 days"));
+            $fim = date("m-d-Y");
+     
+            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $inicio .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+     
+            $dados = json_decode(file_get_contents($url), true);
+     
+            $cotacao = $dados["value"][0]["cotacaoCompra"];
+     
+            $valor_reais = (float) $_REQUEST["valor"] ?? 0;//0 caso valor em branco;
+
+            $valor_dolar = $valor_reais/$cotacao;
+
+            $padrao = numfmt_create("pt_BR",NumberFormatter::CURRENCY);
+            //Utilização de internacionalização monetária (BIBLIOTECA intl)
+
+            echo "<p>VALOR INSERIDO: <strong>".numfmt_format_currency($padrao, $valor_reais, "BRL")."</strong> </p>";
+
+            echo "<p>VALOR EM DÓLAR: <strong>".numfmt_format_currency($padrao, $valor_dolar, "USD")."</strong> </p>";
+
+        ?>
+        <p>
+            <a href="javascript:history.go(-1)">Voltar</a>
+        </p>
+
+    </main>
+    
+    
+</body>
+</html>
